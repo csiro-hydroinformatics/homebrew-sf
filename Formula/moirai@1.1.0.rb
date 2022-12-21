f8443d929e3dbff586eb88ccfc87b3a9929f06cc
J-M <jean-michel.perraud@csiro.au>
2022-12-21 15:57:15 +1100

    Remove comments from brew formula for moirai

--- Formula/moirai@1.1.0.rb
+++ Formula/moirai@1.1.0.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class MoiraiAT110 < Formula
   desc "MOIRAI: Manage C++ Objects's lifetime when exposed through a C API"
   homepage "https://github.com/csiro-hydroinformatics/moirai"
@@ -11,22 +8,12 @@ class MoiraiAT110 < Formula
   depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test moirai@1.1.0`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
